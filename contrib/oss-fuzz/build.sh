f5cf6fd27f1eb308825b471144c7473bdcdecf39
John Taylor <john.taylor@epfl.ch>
2025-05-07 12:11:21 +0200

    Re-enable seed corpus

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -42,9 +42,9 @@ $CXX $CXXFLAGS -std=c++11 -I. \
      -o $OUT/libpng_read_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
-# add seed corpus.
-# find $SRC/libpng -name "*.png" | grep -v crashers | \
-#      xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+add seed corpus.
+find $SRC/libpng -name "*.png" | grep -v crashers | \
+     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
 
-# cp $SRC/libpng/contrib/oss-fuzz/*.dict \
-#      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
+cp $SRC/libpng/contrib/oss-fuzz/*.dict \
+     $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
