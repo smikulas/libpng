bdf1337c760638c210752114c3abc0befa22d43a
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-05-12 23:50:00 +0200

    Temporarily commented transforms for grayscale image

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -179,11 +179,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   }
 
   // Set several transforms that browsers typically use:
-  png_set_gray_to_rgb(png_handler.png_ptr);
+  /*png_set_gray_to_rgb(png_handler.png_ptr);
   png_set_expand(png_handler.png_ptr);
   png_set_packing(png_handler.png_ptr);
   png_set_scale_16(png_handler.png_ptr);
-  png_set_tRNS_to_alpha(png_handler.png_ptr);
+  png_set_tRNS_to_alpha(png_handler.png_ptr);*/
 
   png_color_16 background;
   background.red = 255;
