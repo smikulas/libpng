d71cd4ab446c181e49b3257cecb40619d172c250
John Taylor <john.taylor@epfl.ch>
2025-05-14 18:57:09 +0200

    Reset original harness

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -214,19 +214,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  // image.format = PNG_FORMAT_RGBA;
-  image.format = PNG_FORMAT_RGB_COLORMAP;
+  image.format = PNG_FORMAT_RGBA;
   std::vector<png_byte> buffer(PNG_IMAGE_SIZE(image));
-  
-  // Create a colormap with 256 entries
-  png_color colormap[256];
-  for (int i = 0; i < 256; i++) {
-    colormap[i].red = i;
-    colormap[i].green = i;
-    colormap[i].blue = i;
-  }
-  
-  png_image_finish_read(&image, NULL, buffer.data(), 0, colormap);
+  png_image_finish_read(&image, NULL, buffer.data(), 0, NULL);
 #endif
 
   return 0;
